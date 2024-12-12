96e01200f70bcf90b2f88affd095ad14d3c970e3
Nicolas Guzman <n3kguzman@gmail.com>
2024-12-12 15:12:03 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        byte edad = 31;
+        int numeroGrande = 231231;
+
+
+        System.out.println(numeroGrande);
+    }
+}
